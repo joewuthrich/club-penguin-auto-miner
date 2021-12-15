17a4fe8e60c80a6205b55aac81e966e580fe6317
Joe Wuthrich <joerwuthrich@gmail.com>
2021-12-15 22:09:19 +1300

    Working extension

--- extension/main.js
+++ extension/main.js
@@ -4,39 +4,42 @@ class Controller {
   constructor() {
     this.pos1 = [0, 0];
     this.pos2 = [0, 0];
+    this.currentPos = [0, 0];
     this.loop = false;
   }
 
   setCoordinates() {
+    const posOneListener = (e) => {
+      this.pos1 = [e.clientX, e.clientY];
+    };
+
     const posTwoListener = (e) => {
-      this.setPos2(e);
-      this.click(1838, 1000);
-      this.removeCoordinateListener(posTwoListener);
+      this.pos2 = [e.clientX, e.clientY];
+      this.removeCoordinateListener(posOneListener, posTwoListener);
+      var maxX = Math.max(this.pos1[0], this.pos2[0]);
+      var minX = Math.min(this.pos1[0], this.pos2[0]);
+      var maxY = Math.max(this.pos1[1], this.pos2[1]);
+      var minY = Math.min(this.pos1[1], this.pos2[1]);
+
+      this.currentPos = this.pos2;
+      this.pos1 = [minX, minY];
+      this.pos2 = [maxX, maxY];
+
+      this.startLoop();
     };
 
     console.log("Click and drag to select an area to mine in.");
-    document.addEventListener("mousedown", this.setPos1);
+    document.addEventListener("mousedown", posOneListener);
     document.addEventListener("mouseup", posTwoListener);
   }
 
-  setPos1(e) {
-    this.pos1 = [e.clientX, e.clientY];
-    console.log([e.clientX, e.clientY]);
-  }
-
-  setPos2(e) {
-    this.pos2 = [e.clientX, e.clientY];
-    console.log([e.clientX, e.clientY]);
-  }
-
-  removeCoordinateListener(listener) {
-    document.removeEventListener("mousedown", this.setPos1);
-    document.removeEventListener("mouseup", listener);
+  removeCoordinateListener(listener1, listener2) {
+    document.removeEventListener("mousedown", listener1);
+    document.removeEventListener("mouseup", listener2);
   }
 
   click(x, y) {
-    console.log("clicking at " + x + "," + y);
-    var ev = new MouseEvent("mouseup", {
+    var event = new MouseEvent("mousedown", {
       view: window,
       button: 0,
       bubbles: true,
@@ -45,11 +48,203 @@ class Controller {
       clientY: y,
     });
 
-    console.log(ev.clientX);
+    document.getElementById("cpr_client").dispatchEvent(event);
 
-    document.getElementById("cpr_client").dispatchEvent(ev);
+    event = new MouseEvent("mouseup", {
+      view: window,
+      button: 0,
+      bubbles: true,
+      cancelable: true,
+      clientX: x,
+      clientY: y,
+    });
+
+    document.getElementById("cpr_client").dispatchEvent(event);
+  }
+
+  startLoop() {
+    this.loop = true;
+
+    console.log("Starting automatic loop in two seconds.");
+    setTimeout(() => {
+      this.loopFunction();
+    }, 2000);
+  }
+
+  sleep(min, max) {
+    var ms = this.getRandomRange(min * 1000, max * 1000);
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  async loopFunction() {
+    if (!this.loop) {
+      console.log("Automatic loop stopped.");
+      return;
+    }
+
+    var newPos = [
+      this.getRandomRange(this.currentPos[0] - 35, this.currentPos[0] + 35),
+      this.getRandomRange(this.currentPos[1] - 6, this.currentPos[1] + 55),
+    ];
+
+    var innerX = [this.currentPos[0] - 30, this.currentPos[0] + 30];
+    var innerY = [this.currentPos[1] - 1, this.currentPos[1] + 50];
+
+    while (
+      newPos[0] < this.pos1[0] ||
+      newPos[0] > this.pos2[0] ||
+      newPos[0] == this.currentPos[0] ||
+      newPos[1] < this.pos1[1] ||
+      newPos[1] > this.pos2[1] ||
+      newPos[1] == this.currentPos[1] ||
+      (innerX[0] < newPos[0] && newPos[0] < innerX[1]) ||
+      (innerY[0] < newPos[1] && newPos[1] < innerY[1])
+    ) {
+      newPos = [
+        this.getRandomRange(this.currentPos[0] - 35, this.currentPos[0] + 35),
+        this.getRandomRange(this.currentPos[1] - 6, this.currentPos[1] + 55),
+      ];
+    }
+
+    // var random = Math.random();
+    // var places = [];
+    // if (
+    //   this.isInsideRange(
+    //     [this.currentPos[0] - 35, this.currentPos[1] + 50],
+    //     [this.currentPos[0] + 30, this.currentPos[1] + 55]
+    //   )
+    // ) {
+    //   // top + left
+    //   places.push[0];
+    // }
+
+    // if (
+    //   this.isInsideRange(
+    //     [this.currentPos[0] + 30, this.currentPos[1] - 1],
+    //     [this.currentPos[0] + 35, this.currentPos[1] + 55]
+    //   )
+    // ) {
+    //   // right + top
+    //   places.push[1];
+    // }
+
+    // if (
+    //   this.isInsideRange(
+    //     [this.currentPos[0] - 30, this.currentPos[1] - 1],
+    //     [this.currentPos[0] + 35, this.currentPos[1] - 6]
+    //   )
+    // ) {
+    //   // bottom + right
+    //   places.push[2];
+    // }
+
+    // if (
+    //   this.isInsideRange(
+    //     [this.currentPos[0] - 35, this.currentPos[1] - 30],
+    //     [this.currentPos[0] - 6, this.currentPos[1] + 50]
+    //   )
+    // ) {
+    //   // left + bottom
+    //   places.push[3];
+    // }
+
+    // if (places.length == 0) {
+    //   console.log("IMPOSSIBLE POSITION!");
+    //   return;
+    // }
+
+    // var location = places[math.round(getRandomRange(0, places.length - 1))];
+
+    // if (location == 0) {
+    //   // top + left
+    //   this.currentPos = [
+    //     this.getRandomRange(this.currentPos[0] - 35, this.currentPos[0] + 30),
+    //     this.getRandomRange(this.currentPos[1] + 50, this.currentPos[1] + 55),
+    //   ];
+    // } else if (location == 1) {
+    //   // right + top
+    //   this.currentPos = [
+    //     this.getRandomRange(this.currentPos[0] + 30, this.currentPos[0] + 35),
+    //     this.getRandomRange(this.currentPos[1] - 1, this.currentPos[1] + 55),
+    //   ];
+    // } else if (location == 2) {
+    //   // bottom + right
+    //   this.currentPos = [
+    //     this.getRandomRange(this.currentPos[0] - 30, this.currentPos[0] + 35),
+    //     this.getRandomRange(this.currentPos[1] - 1, this.currentPos[1] - 6),
+    //   ];
+    // } else if (location == 3) {
+    //   // left + bottom
+    //   this.currentPos = [
+    //     this.getRandomRange(this.currentPos[0] - 35, this.currentPos[0] - 30),
+    //     this.getRandomRange(this.currentPos[1] - 6, this.currentPos[1] + 50),
+    //   ];
+    // } else {
+    //   console.log("IMPOSSIBLE POSITION!");
+    //   return;
+    // }
+
+    this.currentPos = newPos;
+
+    console.log(
+      "Moving to " + this.currentPos[0] + ", " + this.currentPos[1] + "."
+    );
+    this.click(...this.currentPos);
+    await this.sleep(0.35, 0.45);
+    var random = Math.random();
+    if (random < 0.005) this.pressKey(83);
+    // s
+    else if (random < 0.01) this.pressKey(87);
+    // w
+    else {
+      if (random > 0.995) {
+        await this.sleep(0.4, 0.7);
+        this.pressKey(72); // h
+        await this.sleep(0.2, 0.4);
+      } else this.pressKey(68); // d
+    }
+
+    await this.sleep(3.3, 3.8);
+
+    this.loopFunction();
+  }
+
+  isInsideRange(coord1, coord2) {
+    return (
+      this.pos1[0] < coord1[0] &&
+      coord1[0] < this.pos2[0] &&
+      this.pos1[0] < coord2[0] &&
+      coord2[0] < this.pos2[0] &&
+      this.pos1[1] < coord1[1] &&
+      coord1[1] < this.pos2[1] &&
+      this.pos1[1] < coord2[1] &&
+      coord2[1] < this.pos2[1]
+    );
+  }
+
+  getRandomRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  stopLoop() {
+    console.log("Stopping automatic loop.");
+    this.loop = false;
+  }
+
+  pressKey(keyCode) {
+    var event = new KeyboardEvent("keydown", {
+      keyCode: keyCode,
+      location: 0,
+      bubbles: true,
+      cancelable: true,
+    });
+
+    document.getElementsByTagName("body")[0].dispatchEvent(event);
   }
 }
 
 const remote = new Controller();
+document.addEventListener("keydown", (event) => {
+  if (event.key == "Escape") remote.stopLoop();
+});
 remote.setCoordinates();
