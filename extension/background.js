df2325f87088106fb23154c553fd9959f060625a
Joe Wuthrich <joerwuthrich@gmail.com>
2021-12-16 13:29:26 +1300

    Extension popup now contains all functions for use

--- extension/background.js
+++ extension/background.js
@@ -2,4 +2,11 @@ browser.runtime.onMessage.addListener((data) => {
   if (data.action == "updateIcon") {
     browser.browserAction.setIcon({ path: data.path });
   }
+  if (data.action == "updateStorage") {
+    console.log("hi");
+    browser.storage.sync.set({
+      pos1: data.pos1,
+      pos2: data.pos2,
+    });
+  }
 });
